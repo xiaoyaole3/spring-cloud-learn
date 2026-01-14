3489820bb3c3b624df7439fef2626ee80712b189
wander <xiaoyaole3@163.com>
2026-01-15 01:18:11 +0800

    增加swagger3依赖的注释信息

--- cloud_01_provider_payment_9001/src/main/java/com/wander/controller/PayController.java
+++ cloud_01_provider_payment_9001/src/main/java/com/wander/controller/PayController.java
@@ -3,11 +3,14 @@ package com.wander.controller;
 import com.wander.dto.PayDTO;
 import com.wander.entity.Pay;
 import com.wander.service.PayService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "支付微服务模块", description = "支付CRUD")
 @RestController
 @RequestMapping("/pay")
 public class PayController {
@@ -15,26 +18,31 @@ public class PayController {
     @Resource
     private PayService payService;
 
+    @Operation(summary = "新增", description = "新增一个订单")
     @PostMapping("/add")
     public Integer add(@RequestBody PayDTO pay) {
         return payService.add(pay);
     }
 
+    @Operation(summary = "删除", description = "删除一个订单")
     @DeleteMapping("/del/{id}")
-    public Integer delete(@PathVariable("id") Integer id) {
+    public Integer delete(@PathVariable(value = "id") Integer id) {
         return payService.delete(id);
     }
 
+    @Operation(summary = "更改", description = "更改一个订单")
     @PutMapping("/update")
     public Integer update(@RequestBody PayDTO pay) {
         return payService.update(pay);
     }
 
+    @Operation(summary = "获取", description = "获取单个订单")
     @GetMapping("/get")
-    public Pay getById(@RequestParam("id") Integer id) {
+    public Pay getById(@RequestParam(value = "id", required = true) Integer id) {
         return payService.getById(id);
     }
 
+    @Operation(summary = "获取全部", description = "获取全部订单")
     @GetMapping("/all")
     public List<Pay> getAll() {
         return payService.getAll();
--- cloud_01_provider_payment_9001/src/main/java/com/wander/entity/Pay.java
+++ cloud_01_provider_payment_9001/src/main/java/com/wander/entity/Pay.java
@@ -1,5 +1,7 @@
 package com.wander.entity;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -11,6 +13,7 @@ import java.util.Date;
  * 表名：t_pay
  * 表注释：支付交易表
 */
+@Schema(title = "支付实体类")
 @Table(name = "t_pay")
 public class Pay {
     @Id
@@ -20,40 +23,47 @@ public class Pay {
     /**
      * 支付流水号
      */
+    @Schema(title = "支付流水号")
     @Column(name = "pay_no")
     private String payNo;
 
     /**
      * 订单流水号
      */
+    @Schema(title = "订单流水号")
     @Column(name = "order_no")
     private String orderNo;
 
     /**
      * 用户账号ID
      */
+    @Schema(title = "用户账号ID")
     @Column(name = "user_id")
     private Integer userId;
 
     /**
      * 交易金额
      */
+    @Schema(title = "交易金额")
     private BigDecimal amount;
 
     /**
      * 删除标志，默认0不删除，1删除
      */
+    @Schema(title = "删除标志，默认0不删除，1删除")
     private Byte deleted;
 
     /**
      * 创建时间
      */
+    @Schema(title = "创建时间")
     @Column(name = "create_time")
     private Date createTime;
 
     /**
      * 更新时间
      */
+    @Schema(title = "更新时间")
     @Column(name = "update_time")
     private Date updateTime;
 
