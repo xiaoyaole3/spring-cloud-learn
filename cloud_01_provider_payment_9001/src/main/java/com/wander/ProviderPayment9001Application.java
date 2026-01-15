97b417970b42c79d06640840f84c2b562e899880
wander <xiaoyaole3@163.com>
2026-01-15 23:55:53 +0800

    调整主启动类带端口号

--- cloud_01_provider_payment_9001/src/main/java/com/wander/ProviderPayment9001Application.java
+++ cloud_01_provider_payment_9001/src/main/java/com/wander/ProviderPayment9001Application.java
@@ -7,8 +7,8 @@ import tk.mybatis.spring.annotation.MapperScan;
 // 注意这里引入的mapperScan需要是tk的，而不能是传统mybatis的
 @MapperScan("com.wander.mapper")
 @SpringBootApplication
-public class ProviderPaymentApplication {
+public class ProviderPayment9001Application {
     public static void main(String[] args) {
-        SpringApplication.run(ProviderPaymentApplication.class, args);
+        SpringApplication.run(ProviderPayment9001Application.class, args);
     }
 }
