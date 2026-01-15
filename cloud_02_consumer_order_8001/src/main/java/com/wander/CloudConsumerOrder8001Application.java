0eae5fece672dda6aa82eeb8d13dad439f4ee5c3
wander <xiaoyaole3@163.com>
2026-01-15 23:55:41 +0800

    初始化一个订单模块

---
+++ cloud_02_consumer_order_8001/src/main/java/com/wander/CloudConsumerOrder8001Application.java
@@ -0,0 +1,11 @@
+package com.wander;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudConsumerOrder8001Application {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudConsumerOrder8001Application.class, args);
+    }
+}
