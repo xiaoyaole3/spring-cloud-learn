32c5dd335908dcd317c0f9361762f2ccdae5ea26
wander <xiaoyaole3@163.com>
2026-01-17 00:58:54 +0800

    将payment服务注册到Consul中

--- cloud_01_provider_payment_9001/src/main/java/com/wander/ProviderPayment9001Application.java
+++ cloud_01_provider_payment_9001/src/main/java/com/wander/ProviderPayment9001Application.java
@@ -2,8 +2,10 @@ package com.wander;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import tk.mybatis.spring.annotation.MapperScan;
 
+@EnableDiscoveryClient
 // 注意这里引入的mapperScan需要是tk的，而不能是传统mybatis的
 @MapperScan("com.wander.mapper")
 @SpringBootApplication
