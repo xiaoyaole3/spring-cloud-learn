5715f840412be7b403b79dfdf8ea8d89aeaf1133
wander <xiaoyaole3@163.com>
2026-01-17 01:10:45 +0800

    Consul默认开启了负载均衡，因此如果想要使用restTemplate，需要给其添加@LoadBalanced注解

--- cloud_02_consumer_order_8001/src/main/java/com/wander/config/RestTemplateConfig.java
+++ cloud_02_consumer_order_8001/src/main/java/com/wander/config/RestTemplateConfig.java
@@ -1,5 +1,6 @@
 package com.wander.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -7,6 +8,8 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class RestTemplateConfig {
 
+    // 注册进入到Consul时需要添加
+    @LoadBalanced
     // 向容器中注入一个RestTemplate操作数据库
     @Bean
     public RestTemplate restTemplate() {
--- cloud_02_consumer_order_8001/src/main/java/com/wander/controller/OrderController.java
+++ cloud_02_consumer_order_8001/src/main/java/com/wander/controller/OrderController.java
@@ -5,7 +5,6 @@ import com.wander.vo.ResultData;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.Resource;
-import org.springframework.beans.BeanUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
@@ -16,7 +15,11 @@ import java.util.List;
 @RequestMapping("/order")
 public class OrderController {
 
-    private static final String PAYMENT_URL = "http://localhost:9001/pay";
+//    private static final String PAYMENT_URL = "http://localhost:9001/pay";
+    // 使用服务注册中心注册的服务名称
+    // 如果只修改这里为注册中心的地址，会提示 ： java.net.UnknownHostException: cloud-provider-payment
+    // 这是因为Consul默认是带负载均衡的，因此在使用restTemplate的时候需要在容器注入时加 @LoadBalanced 注解
+    private static final String PAYMENT_URL = "http://cloud-provider-payment/pay";
 
     @Resource
     private RestTemplate restTemplate;
