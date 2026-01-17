dc017af0f18432dce3584b688486f1128b70b03f
wander <xiaoyaole3@163.com>
2026-01-17 22:20:46 +0800

    openfeign实现了对于服务端的访问，并且带有LB的能力

---
+++ cloud_04_consumer_order_8002_openfeign/src/main/java/com/wander/controller/OrderController.java
@@ -0,0 +1,56 @@
+package com.wander.controller;
+
+import com.wander.apis.PaymentFeignAPI;
+import com.wander.dto.PayDTO;
+import com.wander.vo.ResultData;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.annotation.Resource;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Tag(name = "订单模块", description = "订单用于调用支付模块")
+@RestController
+@RequestMapping("/order/feign")
+public class OrderController {
+
+    @Resource
+    private PaymentFeignAPI paymentFeignAPI;
+
+    @PostMapping("/add")
+    public ResultData<Integer> addOrder(@RequestBody PayDTO payDTO) {
+        return paymentFeignAPI.add(payDTO);
+    }
+
+    @Operation(summary = "删除", description = "删除一个订单")
+    @DeleteMapping("/del/{id}")
+    public ResultData<Integer> delete(@PathVariable(value = "id") Integer id) {
+        return paymentFeignAPI.delete(id);
+    }
+
+    @Operation(summary = "更改", description = "更改一个订单")
+    @PutMapping("/update")
+    public ResultData<Integer> update(@RequestBody PayDTO payDTO) {
+        return paymentFeignAPI.update(payDTO);
+    }
+
+    @Operation(summary = "获取", description = "获取单个订单")
+    @GetMapping("/get")
+    public ResultData<PayDTO> getById(@RequestParam(value = "id", required = true) Integer id) {
+        return paymentFeignAPI.getById(id);
+    }
+
+    @Operation(summary = "获取全部", description = "获取全部订单")
+    @GetMapping("/all")
+    public ResultData<List<PayDTO>> getAll() {
+        return paymentFeignAPI.getAll();
+    }
+
+    // 默认情况下是通过Ribbon的方式负载
+    @Operation(summary = "order中模拟获取Consul中的配置信息", description = "注意这里是使用的客户端")
+    @GetMapping("/consul")
+    public ResultData<String> getConsulConfig() {
+        return paymentFeignAPI.getConsulConfig();
+    }
+}
