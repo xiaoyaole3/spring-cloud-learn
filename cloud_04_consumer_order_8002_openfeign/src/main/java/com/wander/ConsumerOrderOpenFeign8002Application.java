f1b4b250d9c5706bb927e73cb456f42d744875b0
wander <xiaoyaole3@163.com>
2026-01-17 21:34:45 +0800

    初始化一个工程，用于使用openfeign连接服务端

---
+++ cloud_04_consumer_order_8002_openfeign/src/main/java/com/wander/ConsumerOrderOpenFeign8002Application.java
@@ -0,0 +1,11 @@
+package com.wander;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerOrderOpenFeign8002Application {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerOrderOpenFeign8002Application.class, args);
+    }
+}
