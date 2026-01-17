3d1fd2b0b159a44754e852ee5267222ef2d74d2e
wander <xiaoyaole3@163.com>
2026-01-18 00:43:28 +0800

    全局OpenFeign重试机制

---
+++ cloud_04_consumer_order_8002_openfeign/src/main/java/com/wander/config/OpenFeignConfig.java
@@ -0,0 +1,19 @@
+package com.wander.config;
+
+import feign.Retryer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+public class OpenFeignConfig {
+
+    @Bean
+    public Retryer retryer() {
+//        return Retryer.NEVER_RETRY; // 默认情况
+
+        // 最大请求次数为3， 初始间隔为100ms，重试最大间隔时间为1s
+        return new Retryer.Default(100, TimeUnit.SECONDS.toMillis(1L),3);
+    }
+}
