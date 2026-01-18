a46d68bd0a84fb400fcca21db216e1fce80b4a5a
wander <xiaoyaole3@163.com>
2026-01-19 02:03:56 +0800

    基于TIME_BASED的断路器配置测试

--- cloud_04_consumer_order_8002_openfeign/src/main/java/com/wander/config/OpenFeignConfig.java
+++ cloud_04_consumer_order_8002_openfeign/src/main/java/com/wander/config/OpenFeignConfig.java
@@ -12,10 +12,10 @@ public class OpenFeignConfig {
 
     @Bean
     public Retryer retryer() {
-//        return Retryer.NEVER_RETRY; // 默认情况
+        return Retryer.NEVER_RETRY; // 默认情况
 
         // 最大请求次数为3， 初始间隔为100ms，重试最大间隔时间为1s
-        return new Retryer.Default(100, TimeUnit.SECONDS.toMillis(1L),3);
+//        return new Retryer.Default(100, TimeUnit.SECONDS.toMillis(1L),3);
     }
 
     // 开启打印全部日志级别
