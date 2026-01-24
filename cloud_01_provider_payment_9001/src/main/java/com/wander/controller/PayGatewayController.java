0c7ef6a186bf336e4c414816157d69854881d681
wander <xiaoyaole3@163.com>
2026-01-24 15:06:09 +0800

    服务端新增用于网关测试的接口

---
+++ cloud_01_provider_payment_9001/src/main/java/com/wander/controller/PayGatewayController.java
@@ -0,0 +1,35 @@
+package com.wander.controller;
+
+import com.wander.dto.PayDTO;
+import com.wander.entity.Pay;
+import com.wander.service.PayService;
+import com.wander.vo.ResultData;
+import jakarta.annotation.Resource;
+import org.springframework.beans.BeanUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/gateway")
+public class PayGatewayController {
+
+    @Resource
+    private PayService payService;
+
+    @GetMapping("/get")
+    public ResultData<PayDTO> getById(@RequestParam(value = "id") Integer id) {
+        Pay byId = payService.getById(id);
+        PayDTO payDTO = new PayDTO();
+        BeanUtils.copyProperties(byId,payDTO);
+        return ResultData.success(payDTO);
+    }
+
+    @GetMapping("/info")
+    public ResultData<String> getGatewayInfo() {
+        return ResultData.success("Gateway info : " + UUID.randomUUID().toString());
+    }
+}
